07c95650a9337a2459833703da0186e5ce28718d
kyachingprue <kyachingpruemarma.info@gmail.com>
2025-07-21 23:48:35 +0600

    new update commit

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  })
+);
 app.use(express.json());
-
-//jobPortal
-//bE8DEQamMNNCvnHL
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nhw49.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,6 +26,20 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: 'Unauthorized Access' });
+  }
+  jwt.verify(token, process.env.JWT_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 async function run() {
   try {
@@ -37,6 +56,19 @@ async function run() {
       .db('jobPortal')
       .collection('job-applications');
 
+    //Auth JWT Token APIs
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_TOKEN, { expiresIn: '5h' });
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    // Jobs API
     app.get('/jobs', async (req, res) => {
       const job = jobCollection.find();
       const result = await job.toArray();
@@ -50,14 +82,25 @@ async function run() {
       res.send(result);
     });
 
+    app.post('/jobs', async (req, res) => {
+      const newJob = req.body;
+      const result = await jobCollection.insertOne(newJob);
+      res.send(result);
+    });
+
     // Job Application data post via mongodb database
-    app.get('/job-applications', async (req, res) => {
+
+    app.get('/job-applications', verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { applicant_email: email };
+      if (req.user.email !== req.query.email) {
+        return res.status(403).send({ message: 'Forbidden' });
+      }
+      console.log('cuk cuk cookies', req.cookies);
+
       const result = await jobApplicationCollection.find(query).toArray();
       // worse way data loop
       for (const application of result) {
-        console.log(application.job_id);
         const query1 = { _id: new ObjectId(application.job_id) };
         const job = await jobCollection.findOne(query1);
         if (job) {
@@ -73,6 +116,13 @@ async function run() {
     app.post('/job-applications', async (req, res) => {
       const application = req.body;
       const result = await jobApplicationCollection.insertOne(application);
+      res.send(result);
+    });
+
+    app.delete('/job-applications/:id', async (req, res) => {
+      const job = req.params.id;
+      const query = { job_id: job };
+      const result = await jobApplicationCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
